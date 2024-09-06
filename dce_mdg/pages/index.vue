b35095b77e2ddb3e26c83f4d31da7941e6b4fddb
mibressler <53796824+mibressler@users.noreply.github.com>
2024-09-06 14:53:04 +0200

    Create index.vue

---
+++ dce_mdg/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="mx-auto">
+      <iframe
+        src="https://qualtricsxmdfq57lymp.qualtrics.com/jfe/form/SV_08OlC7yH9wV6g8m"
+        class="full-frame"
+      ></iframe>
+  
+    </div>
+  </template>
+  
+  <script setup lang="ts"></script>
+  
+  <style>
+  .main {
+    align-items: center;
+    justify-content: center;
+  }
+  
+  .footer {
+    padding: 20px;
+    text-align: center;
+    color: #8b8b8b;
+  }
+  
+  iframe.full-frame {
+    width: 100vw;
+    height: 100vh;
+    border: none;
+  }
+  </style>
